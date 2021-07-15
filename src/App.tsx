3285e5a1eb8b01c38978a3daed564e7c821e9b44
tg2648 <45052145+tg2648@users.noreply.github.com>
2021-07-15 08:35:15 -0400

    Replace sample code

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,56 @@ import {
   BrowserRouter as Router, Link, Route, Switch
 } from "react-router-dom";
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
 
-export default function BasicExample() {
+type UnitDataType = {
+  unitId: string,
+  unitName: string,
+  conversionFactor: number,
+}
+
+type CategoryDataType = {
+  categoryId: string,
+  categoryName: string,
+  units: UnitDataType[]
+}
+
+const DATA: CategoryDataType[] = [
+  {
+    'categoryId': 'length',
+    'categoryName': 'Length',
+    'units': [
+      {
+        'unitId': 'meters',
+        'unitName': 'Meters',
+        'conversionFactor': 1
+      },
+      {
+        'unitId': 'centimeters',
+        'unitName': 'Centimeters',
+        'conversionFactor': 100
+      },
+      {
+        'unitId': 'inches',
+        'unitName': 'Inches',
+        'conversionFactor': 39.37
+      },
+    ]
+  }
+]
+
+/**
+ * Return conversion factor for a unitId
+ */
+const getConversionFactor = (unitId: string, units: UnitDataType[]): number => {
+  const unitData = units.find(element => element.unitId === unitId);
+  if (unitData) {
+    return unitData.conversionFactor;
+  } else {
+    return 0;
+  }
+}
+
+export default function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <div>
@@ -20,87 +60,32 @@ export default function BasicExample() {
           <li>
             <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
+          {DATA.map((categoryData, index) => {
+            return (
+              <li key={index}>
+                <Link to={'/' + categoryData.categoryId}>{categoryData.categoryName}</Link>
+              </li>
+            )
+          })}
         </ul>
 
         <hr />
 
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
           <Route exact path="/">
-            <Area />
+            <div>Home</div>
           </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
+          {DATA.map((categoryData, index) => {
+            return (
+              <Route key={index} path={'/' + categoryData.categoryId}>
+                <Category data={categoryData} />
+              </Route>
+            )
+          })}
         </Switch>
       </div>
     </Router>
-  );
-}
-
-// You can think of these components as "pages"
-// in your app.
-
-function Area() {
-  type ValueType = {
-    [index: string]: string,
-  }
-
-  const [values, setValues] = useState<ValueType>({ 'meters': '', 'inches': '' });
-
-  type ConversionType = {
-    [index: string]: number,
-  }
-
-  const conversionFactors: ConversionType = {
-    'meters': 1,
-    'inches': 39.37,
-  }
-
-  const handleChange = (id: 'meters' | 'inches', e: ChangeEvent<HTMLInputElement>) => {
-
-    let newValues: ValueType = {
-      'meters': '',
-      'inches': '',
-    }
-
-    if (e.target.value) {
-      const commonValue = 1 / conversionFactors[id] * Number.parseInt(e.target.value);
-      // console.log('commonValue', commonValue);
-
-      for (const unit in newValues) {
-        if (unit === id) {
-          newValues[unit] = e.target.value;
-        } else {
-          newValues[unit] = Number(commonValue * conversionFactors[unit]).toFixed(2).toString();
-        }
-      }
-    }
-
-    setValues(newValues);
-  }
-
-  return (
-    <>
-      <Input label="Meters" value={values.meters} id="meters" handleChange={(e: ChangeEvent<HTMLInputElement>) => handleChange('meters', e)} />
-      <Input label="Inches" value={values.inches} id="inches" handleChange={(e: ChangeEvent<HTMLInputElement>) => handleChange('inches', e)} />
-    </>
-  );
+  )
 }
 
 type InputPropsType = {
@@ -109,7 +94,6 @@ type InputPropsType = {
   id: string,
   handleChange: (e: ChangeEvent<HTMLInputElement>) => void
 }
-
 const Input: React.FC<InputPropsType> = (props) => {
 
   return (
@@ -120,18 +104,65 @@ const Input: React.FC<InputPropsType> = (props) => {
   )
 }
 
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
 
-function Dashboard() {
+type CategoryPropsType = {
+  data: CategoryDataType
+}
+const Category: React.FC<CategoryPropsType> = (props) => {
+
+  type StateValueType = {
+    [index: string]: string,
+  }
+  const initialValues: StateValueType = {}
+  props.data.units.forEach(unit => {
+    initialValues[unit.unitId] = ''
+  });
+
+  const [values, setValues] = useState(initialValues);
+
+  /**
+   * onChange event handler
+   * @param changedUnitId id of the changed unit
+   * @param e change event
+   */
+  const handleChange = (changedUnitId: string, e: ChangeEvent<HTMLInputElement>) => {
+    console.log('onChange in ', changedUnitId);
+
+    let newValues = {...initialValues};
+
+    if (e.target.value) {
+      const commonValue = 1 / getConversionFactor(changedUnitId, props.data.units) * Number.parseInt(e.target.value);
+
+      for (const unitId in newValues) {
+        if (unitId === changedUnitId) {
+          newValues[unitId] = e.target.value;
+        } else {
+          newValues[unitId] = Number(commonValue * getConversionFactor(unitId, props.data.units)).toFixed(2).toString();
+        }
+      }
+    }
+
+    setValues(newValues);
+  }
+
   return (
     <div>
-      <h2>Dashboard</h2>
+      {
+        props.data.units.map((unit, index) => {
+          return (
+            <div key={index}>
+              <Input
+                label={unit.unitName}
+                value={values[unit.unitId]}
+                id={unit.unitId}
+                handleChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(unit.unitId, e)}
+              />
+              <br></br>
+            </div>
+          )
+        })
+      }
     </div>
-  );
+  )
+
 }
