6ea239ab887e1127dc725fd93bce59165670da6f
tg2648 <45052145+tg2648@users.noreply.github.com>
2021-07-15 16:28:21 -0400

    Add bootstrap styling to home page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 import {
   BrowserRouter as Router, Link, Route, Switch
 } from "react-router-dom";
@@ -8,18 +11,26 @@ import InputGroup from "./InputGroup";
 
 export default function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Container>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/">
-            <ul>
+            <div className='d-grid gap-2 col-lg-5 mx-auto'>
+              <h1 className="display-6">Unit Converter</h1>
+              {/* <ButtonGroup vertical> */}
               {DATA.map((categoryData, index) => {
                 return (
-                  <li key={index}>
-                    <categoryData.categoryIcon /><Link to={'/' + categoryData.categoryId}>{categoryData.categoryName}</Link>
-                  </li>
+                  <LinkContainer key={index} to={'/' + categoryData.categoryId}>
+                    <Button size="lg">
+                      <categoryData.categoryIcon />
+                      {' '}
+                      {categoryData.categoryName}
+                    </Button>
+                  </LinkContainer>
                 )
               })}
-            </ul>
+              {/* </ButtonGroup> */}
+            </div>
           </Route>
 
           {DATA.map((categoryData, index) => {
@@ -32,6 +43,7 @@ export default function App() {
           })}
 
         </Switch>
-    </Router>
+      </Router>
+    </Container>
   )
 }
