e8a66d4dd0e09527fa5dcfa60a570ac864996e16
tg2648 <45052145+tg2648@users.noreply.github.com>
2021-07-15 22:56:19 -0400

    Add inputMode to input tags

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,7 +9,7 @@ type InputProps = {
 const Input: React.FC<InputProps> = (props) => {
 
   return (
-    <input type="text" className="form-control" value={props.value} onChange={props.handleChange} />
+    <input type="text" inputMode="decimal" className="form-control" value={props.value} onChange={props.handleChange} />
   )
 }
 
